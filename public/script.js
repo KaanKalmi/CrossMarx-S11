262669d576ce0eda978b9e077dc9c92e3c82c259
RubenErhardt <rubengamebattles@outlook.com>
2024-05-16 21:37:01 +0200

    Client side + Index + Style effect
    - Client side effect
    - Index benaming aanpassen
    - Css toegevoegd
    ( Moet nog namen goedmaken in index)

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+
 const togglePasswordIcons = document.querySelectorAll('.toggle-password');
 togglePasswordIcons.forEach(icon => {
     icon.addEventListener('click', () => {
@@ -11,5 +12,18 @@ togglePasswordIcons.forEach(icon => {
             icon.classList.remove('fa-eye-slash');
             icon.classList.add('fa-eye');
         }
+    });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    const loginButton = document.querySelector('.login-btn');
+    const loginForm = document.querySelector('.Login-form');
+    const registerForm = document.querySelector('.Register-form');
+
+    loginButton.addEventListener('click', function() {
+        loginForm.classList.add('show'); // Move the login form into view
+        registerForm.classList.add('hide');
+        setTimeout(() => {
+            registerForm.classList.add('hidden');
+        }, 1000); // Adjust the delay time as needed
     });
 });
