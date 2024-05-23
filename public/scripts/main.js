db455bf07665bfb4d105457a0cb734dc9c311634
RubenErhardt <rubengamebattles@outlook.com>
2024-05-23 21:33:48 +0200

    CSS button sound
    - button sound geadd
    - eindelijk werkt t

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,7 @@
-console.log('js werkt!!')
+const button = document.getElementById("overslaan-button");
+const audio = document.getElementById("clickSound");
+
+button.addEventListener("click", () => {
+  audio.currentTime = 0; // Reset playback position to avoid glitches
+  audio.play();
+});
